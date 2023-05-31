ee60b66103af7025e451f93fc9072250ded57b0a
SJ <samuel.jaja21@gmail.com>
2023-05-31 16:39:59 +0100

    Added HanldeBars as View Engine for Node Project.

--- express-server/controllers/message.controller.js
+++ express-server/controllers/message.controller.js
@@ -1,6 +1,13 @@
+//const path = require('path');
+
 function getMessages(req, res){
     //res.send("Hello Node.js")
-    res.send('<ul><li>Hello Node.js</li></ul>');
+    //res.send('<ul><li>Hello Node.js</li></ul>');
+    //res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg' ))
+    res.render('messages', {
+        title:'Messages to my Friends!',
+        friend: 'Murphy Ochuba',
+    });
 }
 
 function postMessages(req, res){
--- express-server/server.js
+++ express-server/server.js
@@ -1,10 +1,13 @@
-//import express from 'express'; ES2020
-const express = require('express')
-
+//Using ES2020 import express from 'express';
+const express = require('express');
+const path = require('path');
 const friendsRouter = require('./routes/friends.routes');
-const messageRouter = require('./routes/message.routes')
+const messageRouter = require('./routes/message.routes');
 
 const app = express();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 const PORT = 4001;
 
 app.use((req, res, next)=>{
@@ -14,11 +17,18 @@ app.use((req, res, next)=>{
     console.log(`${req.method} ${req.baseUrl}${req.url} ${delta} milliseconds [ms]`);
 })
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
+app.get('/', (req,res) => {
+    res.render('index', {
+        title: 'Welcome to CypherCrescent',
+        caption: 'Let\'s go Coding with Node.js @CypherCrescent ',
+    })
+})
 app.use('/friends', friendsRouter);
 app.use('/message', messageRouter);
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
 })
